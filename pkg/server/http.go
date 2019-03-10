548f6c1befb2b86acb8f1a3202caff3228d267b9
Taufan Mahaputra <hacker.indo62@gmail.com>
2019-03-11 02:50:33 +0700

    Add route post new rate

--- pkg/server/http.go
+++ pkg/server/http.go
@@ -2,6 +2,7 @@ package server
 
 
 import (
+	"github.com/taufanmahaputra/forex/pkg/controller"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -17,16 +18,35 @@ func NewHttpServer() HttpService {
 
 func (s HttpService) RegisterHandler(e *echo.Echo) {
 	e.GET("/", index)
+	e.POST("api/v1/rate", handleNewRate)
 }
 
 type Response struct {
 	Title  string `json:"title"`
-	Status string `json:"status"`
+	Message string `json:"message"`
 }
 
-func index(c echo.Context) error {
-	return c.JSON(http.StatusOK, Response{
+func index(ctx echo.Context) error {
+	return handleReponse(ctx, http.StatusOK, Response{
 		"Foreign exchange rate API",
 		"OK",
 	})
+}
+
+func handleNewRate(ctx echo.Context) error {
+	rate := new(controller.ExchangeRate)
+	if err := ctx.Bind(rate); err != nil {
+		return handleReponse(ctx, http.StatusBadRequest, Response{Message: "Invalid payload"})
+	}
+
+	err := rateController.PutNewExchangeRate(*rate)
+	if err != nil {
+		return handleReponse(ctx, http.StatusInternalServerError, Response{Message: "Internal server error"})
+	}
+
+	return handleReponse(ctx, http.StatusCreated, rate)
+}
+
+func handleReponse(ctx echo.Context, statusCode int, response interface{}) error {
+	return ctx.JSON(statusCode, response)
 }
