cc8d1dabde07c2307ba5c894f9d5214abacb26d2
Taufan Mahaputra <hacker.indo62@gmail.com>
2019-03-11 02:50:02 +0700

    Add logger and cors middleware to echo

--- cmd/forex/main.go
+++ cmd/forex/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/taufanmahaputra/forex/pkg/server"
+	"log"
 )
 
 func main()  {
 	e := echo.New()
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+
 	httpServer := server.NewHttpServer()
 	httpServer.RegisterHandler(e)
+
+	err := server.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	e.Logger.Fatal(e.Start(":9999"))
 }
